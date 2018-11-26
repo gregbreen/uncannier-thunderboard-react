ed4546a4bfb0a69e835e77f7d2887347291fea1d
Greg Breen <greg@uncannier.com>
2018-11-26 21:04:24 +0800

    README updates

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -3,4 +3,4 @@ curl --user ${CIRCLE_TOKEN}: \
     --form revision=$1\
     --form config=@config.yml \
     --form notify=false \
-        https://circleci.com/api/v1.1/project/github/gregbreen/uncannier-thunderboard-react/tree/task_8-CircleCI
+        https://circleci.com/api/v1.1/project/github/gregbreen/uncannier-thunderboard-react/tree/$2
